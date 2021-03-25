b1d6cff2963ae88293865ce5d767766a6590db6e
shahardc <81380081+shahardc@users.noreply.github.com>
2021-03-25 19:53:40 +0200

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,4 +1,22 @@
 package algorithms.mazeGenerators;
 
 public class Maze {
+    private int rows;
+    private int columns;
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
 }
