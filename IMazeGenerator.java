388fc7987fe40ce660c540f2bf5fc43ad6c0ef90
shahardc <81380081+shahardc@users.noreply.github.com>
2021-03-25 19:53:08 +0200

    Update IMazeGenerator.java

--- IMazeGenerator.java
+++ IMazeGenerator.java
@@ -1,8 +1,6 @@
 package algorithms.mazeGenerators;
 
 public interface IMazeGenerator {
-    public static Maze generate(int rows, int columns)
-    {
-
-    }
+    public Maze generate(int rows, int columns);
+    public long measureAlgorithmTimeMillis(int rows, int columns);
 }
