d4eb261b9e35ce580196fe34d5bec1f42fa198c5
shahardc <81380081+shahardc@users.noreply.github.com>
2021-03-30 22:18:59 +0300

    Update MyMazeGenerator.java

--- MyMazeGenerator.java
+++ MyMazeGenerator.java
@@ -117,16 +117,7 @@ public class MyMazeGenerator extends AMazeGenerator{
     public Maze generate(int rows, int columns) {
         if(rows<1|columns<1)
             return null;
-        Maze m=new Maze();
-        m.setRows(rows);
-        m.setColumns(columns);
-        int[][] map=new int[rows][columns];
-        for(int i=0;i<rows;i++) {
-            for (int j = 0; j < columns; j++) {
-                map[i][j] = 1;
-            }
-        }
-        m.setWalls(map);
+        Maze m=EmptyMazeGenerator.generate1(rows,columns);
         Stack<Position> Pathstk= new Stack<>();
         m.setStart(new Position(0,0));
         Pathstk.push(new Position(0,0));
