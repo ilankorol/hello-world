33a202a45543aa1531b8ddb5a494db490ee65f06
shahardc <81380081+shahardc@users.noreply.github.com>
2021-03-30 19:37:38 +0300

    Update SimpleMazeGenerator.java

--- SimpleMazeGenerator.java
+++ SimpleMazeGenerator.java
@@ -2,6 +2,117 @@ package algorithms.mazeGenerators;
 import java.util.*;
 
 public class SimpleMazeGenerator extends AMazeGenerator{
+
+    public static boolean CheckUpNeighbor(Maze m,Position p)
+    {
+        boolean upValid=false;
+        int row=p.getRowIndex();
+        int col=p.getColumnIndex();
+        if (row - 1 >= 0) {
+            upValid=true;
+            if(row-2>=0)
+                if(m.getWalls()[row-2][col]==0) {
+                    upValid = false;
+                }
+            if(col-1>=0)
+                if(m.getWalls()[row-1][col-1]==0) {
+                    upValid = false;
+                }
+            if(col+1<m.getColumns())
+                if(m.getWalls()[row-1][col+1]==0) {
+                    upValid = false;
+                }
+        }
+        return upValid;
+    }
+    public static boolean CheckDownNeighbor(Maze m,Position p)
+    {
+        boolean downValid=false;
+        int row=p.getRowIndex();
+        int col=p.getColumnIndex();
+        if (row + 1 < m.getRows()) {
+            downValid=true;
+            if(row+2<m.getRows())
+                if(m.getWalls()[row+2][col]==0) {
+                    downValid = false;
+                }
+            if(col-1>=0)
+                if(m.getWalls()[row+1][col-1]==0) {
+                    downValid = false;
+                }
+            if(col+1<m.getColumns())
+                if(m.getWalls()[row+1][col+1]==0) {
+                    downValid = false;
+                }
+        }
+        return downValid;
+    }
+    public static boolean CheckLeftNeighbor(Maze m,Position p)
+    {
+        boolean leftValid=false;
+        int row=p.getRowIndex();
+        int col=p.getColumnIndex();
+        if (col - 1 >= 0) {
+            leftValid=true;
+            if(col-2>=0)
+                if(m.getWalls()[row][col-2]==0) {
+                    leftValid = false;
+                }
+            if(row-1>=0)
+                if(m.getWalls()[row-1][col-1]==0) {
+                    leftValid = false;
+                }
+            if(row+1<m.getRows())
+                if(m.getWalls()[row+1][col-1]==0) {
+                    leftValid = false;
+                }
+        }
+        return leftValid;
+    }
+    public static boolean CheckRightNeighbor(Maze m,Position p)
+    {
+        boolean rightValid=false;
+        int row=p.getRowIndex();
+        int col=p.getColumnIndex();
+        if (col + 1 < m.getColumns()) {
+            rightValid=true;
+            if(col+2 < m.getColumns())
+                if(m.getWalls()[row][col+2]==0) {
+                    rightValid = false;
+                }
+            if(row-1>=0)
+                if(m.getWalls()[row-1][col+1]==0) {
+                    rightValid = false;
+                }
+            if(row+1<m.getRows())
+                if(m.getWalls()[row+1][col+1]==0) {
+                    rightValid = false;
+                }
+        }
+        return rightValid;
+    }
+    public static Stack<Position> GetAvailableNeighbors(Maze m, Position p) {
+        if (p == null)
+            return null;
+        Stack<Position> k = new Stack<>();
+        if (SimpleMazeGenerator.CheckUpNeighbor(m, p)) {
+            Position pos = new Position(p.getRowIndex() - 1, p.getColumnIndex());
+            k.push(pos);
+        }
+        if (SimpleMazeGenerator.CheckDownNeighbor(m, p)) {
+            Position pos = new Position(p.getRowIndex() + 1, p.getColumnIndex());
+            k.push(pos);
+        }
+        if (SimpleMazeGenerator.CheckLeftNeighbor(m, p)) {
+            Position pos = new Position(p.getRowIndex(), p.getColumnIndex() - 1);
+            k.push(pos);
+        }
+        if (SimpleMazeGenerator.CheckRightNeighbor(m, p)) {
+            Position pos = new Position(p.getRowIndex(), p.getColumnIndex() + 1);
+            k.push(pos);
+        }
+        return k;
+    }
     @Override
     public Maze generate(int rows, int columns) {
         if(rows<1|columns<1)
@@ -17,18 +128,18 @@ public class SimpleMazeGenerator extends AMazeGenerator{
         }
         m.setWalls(map);
         Stack<Position> Pathstk= new Stack<>();
-        int randRow=(int)Math.floor(Math.random() * rows);
-        int randCol=(int)Math.floor(Math.random() * columns);
-        Pathstk.push(new Position(randRow,randCol));
+        m.setStart(new Position(0,0));
+        Pathstk.push(new Position(0,0));
+        Position randNeighborPos = null;//ignore this line-only for no errors at push(152)
         while(!Pathstk.isEmpty())
         {
             Position currCell = Pathstk.pop();
-            Stack<Position> Neighbors=m.GetAvailableNeighbors(currCell);
+            Stack<Position> Neighbors=SimpleMazeGenerator.GetAvailableNeighbors(m,currCell);
             if (!Neighbors.isEmpty())
             {
-                Position randNeighborPos = null;//ignore this line-only for no errors at push(42)
                 int randNeighborInd=(int)Math.floor(Math.random() * (Neighbors.size()));
-                for (int i = 0; i < Neighbors.size(); ++i)
+                int NSize=Neighbors.size();
+                for (int i = 0; i < NSize; ++i)
                 {
                     Position CurrNeighbor= Neighbors.pop();
                     if(i==randNeighborInd) {
@@ -42,7 +153,16 @@ public class SimpleMazeGenerator extends AMazeGenerator{
                 Pathstk.push(randNeighborPos);
             }
         }
-        //need to add here a start and end positions to m
+        int randRow=(int)Math.floor(Math.random() * rows);
+        int randCol=(int)Math.floor(Math.random() * columns);
+        m.setEnd(null);
+        while (m.getEnd()==null)
+        {
+            int halfRowsAndCols=(m.getRows()+m.getColumns())/2;//for making interesting mazes
+            Position pos = new Position(randRow, randCol);
+            if(randRow+randCol>=halfRowsAndCols & m.getWalls()[randCol][randRow]==0)
+                m.setEnd(pos);
+        }
         return m;
     }
 }
