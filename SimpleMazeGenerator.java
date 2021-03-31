9d6704b4a8c6eaac3168cbbf4660177b67adcfa6
shahardc <81380081+shahardc@users.noreply.github.com>
2021-03-31 14:51:29 +0300

    Update SimpleMazeGenerator.java

--- SimpleMazeGenerator.java
+++ SimpleMazeGenerator.java
@@ -1,57 +1,34 @@
 package algorithms.mazeGenerators;
-import java.util.*;
 
-public class SimpleMazeGenerator extends AMazeGenerator{
+public class SimpleMazeGenerator extends AMazeGenerator {
 
     public Maze generate(int rows, int columns) {
-        Maze m=EmptyMazeGenerator.generate1(rows,columns);
+        Maze m= EmptyMazeGenerator.generate1(rows,columns);
         int UnableToGetToCells=rows*columns;
         m.setStart(new Position(0,0));
         m.setCell(0,0,0);
-        while(UnableToGetToCells>0) {
-            UnableToGetToCells=0;
-            for (int currRow = 0; currRow < rows; currRow++) {
-                for (int currCol = 0; currCol < columns; currCol++) {
-                    boolean Unable = true;
-                    if (m.getWalls()[currRow][currCol] == 0)
-                        Unable = false;
-                    if (currRow - 1 >= 0)
-                    {
-                        if(m.getWalls()[currRow - 1][currCol] == 0)
-                            Unable = false;
-                    }
-                    if (currCol - 1 >= 0)
-                    {
-                        if(m.getWalls()[currRow][currCol - 1] == 0)
-                            Unable = false;
-                    }
-                    if (currRow + 1 < rows)
-                    {
-                        if(m.getWalls()[currRow + 1][currCol] == 0)
-                            Unable = false;
-                    }
-                    if (currCol + 1 < columns )
-                    {
-                        if(m.getWalls()[currRow][currCol + 1] == 0)
-                            Unable = false;
-                    }
-                    if (Unable) {
-                        UnableToGetToCells++;
-                        m.setCell(currRow, currCol, (int) Math.floor(Math.random() * 2));
-                    }
-                }
+        for (int currRow = 0; currRow < rows; currRow++) {
+            for (int currCol = 0; currCol < columns; currCol++) {
+                if((int) Math.floor(Math.random() * 5)<3)
+                    m.setCell(currRow, currCol,0);
             }
         }
         int randRow=(int)Math.floor(Math.random() * rows);
         int randCol=(int)Math.floor(Math.random() * columns);
-        m.setEnd(null);
-        while (m.getEnd()==null)
+        while(randRow==0&&randCol==0)
         {
-            int halfRowsAndCols=(m.getRows()+m.getColumns())/2;//for making interesting mazes
-            Position pos = new Position(randRow, randCol);
-            if(randRow+randCol>=halfRowsAndCols & m.getWalls()[randCol][randRow]==0)
-                m.setEnd(pos);
+            randRow=(int)Math.floor(Math.random() * rows);
+            randCol=(int)Math.floor(Math.random() * columns);
         }
+        for(int col=0;col<=randCol;col++)
+        {
+            m.setCell(0,col,0);
+        }
+        for(int row=0;row<=randRow;row++)
+        {
+            m.setCell(row,randCol,0);
+        }
+        m.setEnd(new Position(randRow,randCol));
         return m;
 
     }
