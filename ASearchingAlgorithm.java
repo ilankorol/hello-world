00990280c6fb1a601d2a19326da4847bccdccdbb
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-06 19:32:52 +0300

    Update ASearchingAlgorithm.java

--- ASearchingAlgorithm.java
+++ ASearchingAlgorithm.java
@@ -1,4 +1,54 @@
 package algorithms.search;
 
-public class ASearchingAlgorithm {
+import algorithms.mazeGenerators.Position;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public abstract class ASearchingAlgorithm implements ISearchingAlgorithm{
+    protected PriorityQueue<AState> openList;
+    protected PriorityQueue<AState> closedList;
+    private int visitedNodes;
+
+    private boolean MyContains(PriorityQueue<AState> q,AState s)
+    {
+        AState curr;
+        Iterator<AState> iter=q.iterator();
+        while(iter.hasNext())
+        {
+            curr=iter.next();
+            if(curr.compareTo(s)==0)
+                return true;
+        }
+        return false;
+    }
+    public boolean openContains(AState s)
+    {
+        return MyContains(openList,s);
+    }
+    public boolean closedContains(AState s)
+    {
+        return MyContains(closedList,s);
+    }
+
+    public void setVisitedNodes(int visitedNodes) {
+        this.visitedNodes = visitedNodes;
+    }
+
+    public ASearchingAlgorithm(){
+        openList=new PriorityQueue<AState>();
+        closedList=new PriorityQueue<AState>();
+        visitedNodes=0;
+    }
+    protected AState popOpenList(){
+        visitedNodes++;
+        return openList.poll();
+    }
+
+    @Override
+    public int getNumberOfNodesEvaluated() {
+        return visitedNodes;
+    }
+
 }
