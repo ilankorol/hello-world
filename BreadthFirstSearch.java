b3c68c112de21e8d4e24cc24c8b1c3e320e84daf
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-06 19:33:39 +0300

    Update BreadthFirstSearch.java

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -1,4 +1,13 @@
 package algorithms.search;
 
-public class BreadthFirstSearch {
+public class BreadthFirstSearch extends BestFirstSearch{
+    @Override
+    public Solution solve(ISearchable s) {
+        return super.solve(s);
+    }
+
+    @Override
+    public String getName() {
+        return "BreadthFirstSearch";
+    }
 }
