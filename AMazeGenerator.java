283a681138560815dab4069c10391ad5f3450cb2
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-19 04:33:41 +0300

    Update AMazeGenerator.java

--- AMazeGenerator.java
+++ AMazeGenerator.java
@@ -2,13 +2,15 @@ package algorithms.mazeGenerators;
 
 public abstract class AMazeGenerator implements IMazeGenerator {
     @Override
-    public long measureAlgorithmTimeMillis(int rows, int columns) {
-        if(rows<1||columns<1)
-            return -1;
+    public long measureAlgorithmTimeMillis(int rows, int columns) throws Exception {
+        if(rows<2)
+            throw new Exception("Not Enough Rows");
+        if(columns<2)
+            throw new Exception("Not Enough Columns");
         long before=System.currentTimeMillis();
         generate(rows,columns);
         long after=System.currentTimeMillis();
         return (long)after-before;
     }
-    public abstract Maze generate(int rows, int columns);
+    public abstract Maze generate(int rows, int columns) throws Exception;
 }
