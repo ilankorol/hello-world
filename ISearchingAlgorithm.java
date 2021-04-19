ca310ac3dc7605ba82d794fdb047a8a55124dbe3
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-19 04:36:51 +0300

    Update ISearchingAlgorithm.java

--- ISearchingAlgorithm.java
+++ ISearchingAlgorithm.java
@@ -1,7 +1,7 @@
 package algorithms.search;
 
 public interface ISearchingAlgorithm {
-    Solution solve(ISearchable s);
-    int getNumberOfVisitedNodes();
+    Solution solve(ISearchable s) throws Exception;
+    int getNumberOfNodesEvaluated();
     String getName();
 }
