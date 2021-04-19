904e468619568bfbd0c83e1cc8cdc8d3f1fa04eb
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-19 04:31:23 +0300

    Add files via upload

---
+++ Maze3DNode.java
@@ -0,0 +1,10 @@
+package algorithms.maze3D;
+
+import algorithms.mazeGenerators.Position;
+import algorithms.search.Node;
+
+public class Maze3DNode extends Node {
+    public Maze3DNode(Position3D data) throws Exception {
+        super(data);
+    }
+}
