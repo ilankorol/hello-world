dba91db3adbfc17d49e0fadf6184902e40df1a8b
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-19 04:38:05 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -5,11 +5,21 @@ import java.util.ArrayList;
 public class Solution {
     private ArrayList<AState> SolutionPath;
  
-    public Solution(ArrayList<AState> solutionPath) {
+    public Solution(ArrayList<AState> solutionPath) throws Exception {
+        if(solutionPath==null)
+            throw new Exception("Path Is null");
         SolutionPath = solutionPath;
     }
 
     public ArrayList<AState> getSolutionPath(){
         return SolutionPath;
+    }
+
+    public void print()
+    {
+        ArrayList<AState> solutionPath = getSolutionPath();
+        for (int i = 0; i < solutionPath.size(); i++) {
+            System.out.println(String.format("%s.%s", i, solutionPath.get(i)));
+        }
     }
 }
