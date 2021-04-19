85b79dd23ebbe1ddb68db2f419e11a5b12d57094
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-19 04:37:14 +0300

    Update MazeNode.java

--- MazeNode.java
+++ MazeNode.java
@@ -1,7 +1,10 @@
 package algorithms.search;
 
-public interface ISearchingAlgorithm {
-    Solution solve(ISearchable s) throws Exception;
-    int getNumberOfNodesEvaluated();
-    String getName();
+import algorithms.mazeGenerators.Position;
+
+public class MazeNode extends Node{
+
+    public MazeNode(Position c) throws Exception {
+        super(c);
+    }
 }
