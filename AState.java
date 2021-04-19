18c6a92692770ee630bb95dd2cd50385ebd24f6c
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-19 04:35:47 +0300

    Update AState.java

--- AState.java
+++ AState.java
@@ -1,39 +1,49 @@
 package algorithms.search;
 
 public abstract class AState implements Comparable<AState> {
-    AState PreviousState;
-    Node node;
-    int cost;
+    protected AState PreviousState;
+    protected Node node;
+    protected int cost;
+    protected int AmountOfPreviousStates;
     public AState getPreviousState() {
         return PreviousState;
     }
-
-    public Node getNode() {
-        return node;
+    public Node getNode()
+    {
+        return this.node;
     }
 
     public int getCost() {
         return cost;
     }
 
-    public AState(AState previousState, Node node, int cost) {
+    public AState(AState previousState, Node node, int cost) throws Exception {
+        if(node==null)
+            throw new Exception("node is null");
+        if(cost<0)
+            throw new Exception("No Negative Cost");
         PreviousState = previousState;
         this.node = node;
         this.cost = cost;
+        if(previousState!=null)
+            AmountOfPreviousStates=previousState.AmountOfPreviousStates+1;
+        else
+            AmountOfPreviousStates=0;
     }
 
     public void setPreviousState(AState previousState) {
         this.PreviousState = previousState;
     }
 
-    public void setNode(Node node) {
+    public void setNode(Node node) throws Exception {
+        if(node==null)
+            throw new Exception("node is null");
         this.node = node;
     }
 
-    public void setCost(int cost) {
+    public void setCost(int cost) throws Exception {
         this.cost = cost;
     }
-    //public abstract int compare(AState a);
     @Override
     public abstract int compareTo(AState o);
     @Override
