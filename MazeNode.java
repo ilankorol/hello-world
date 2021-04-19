bfc89225a1dda7c2e3a3f72303cfb4da40c1bcfe
shahardc <81380081+shahardc@users.noreply.github.com>
2021-04-19 04:37:22 +0300

    Update MazeNode.java

--- MazeNode.java
+++ MazeNode.java
@@ -2,9 +2,21 @@ package algorithms.search;
 
 import algorithms.mazeGenerators.Position;
 
-public class MazeNode extends Node{
+public class MazeState extends AState{
 
-    public MazeNode(Position c) throws Exception {
-        super(c);
+    public MazeState(AState previousState, Node node, int cost) throws Exception {
+        super(previousState, node, cost);
     }
+
+    @Override
+    public int compareTo(AState o) {
+        if(((Position)this.node.getData()).getRowIndex()==((Position)o.node.getData()).getRowIndex()&&
+                ((Position)this.node.getData()).getColumnIndex()==((Position)o.node.getData()).getColumnIndex())
+            return 0;
+        return -1;
+    }
+
+    @Override
+    public String toString() {return node.getData().toString();}
+
 }
