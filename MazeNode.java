997627395b27dbc4c023a90cf94a1b8b92ee8ad7
shahardc <81380081+shahardc@users.noreply.github.com>
2021-05-12 00:50:45 +0300

    Update MazeNode.java

--- MazeNode.java
+++ MazeNode.java
@@ -2,7 +2,7 @@ package algorithms.search;
 
 import algorithms.mazeGenerators.Position;
 
-public class MazeNode extends Node{
+public class MazeNode extends Node implements java.io.Serializable{
 
     public MazeNode(Position c) throws Exception {
         super(c);
